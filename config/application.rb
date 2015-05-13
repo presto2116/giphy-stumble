8248ee0503041c9b082ac19877cbe701eef4029d
presto2116 <preston.straight@gmail.com>
2015-05-13 14:13:13 -0400

    heroku probs

--- config/application.rb
+++ config/application.rb
@@ -17,9 +17,10 @@ Bundler.require(*Rails.groups)
 
 module GiphyStumble
   class Application < Rails::Application
+    
     config.assets.initialize_on_precompile = false
 
-    config.serve_static_assets = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
